17a4817c5fa9d1343cfd57d2857d280be53e1291
Renata Soares <renatasoaresdorego@gmail.com>
2024-05-13 18:39:12 -0300

    Criação modal portico

--- canva.js
+++ canva.js
@@ -8,17 +8,6 @@ img.onload = function () {
     ctx.drawImage(img, 0, 0, canvas.clientWidth, canvas.height);
 }
 
-// Desenha elementos interativos sobre a imagem
-// Exemplo de elemento interativo
-ctx.fillStyle = 'rgba(255, 0, 0, 0.75)';
-ctx.fillRect(326, 98, 43, 21);
-
-// Adicione mais elementos interativos conforme necessário
-ctx.fillStyle = 'rgba(173, 202, 52, 0.75)';
-ctx.fillRect(551, 98, 43, 21);
-
-
-
 // Adiciona eventos de clique aos elementos interativos
 canvas.addEventListener('click', function (event) {
     var rect = canvas.getBoundingClientRect();
@@ -26,17 +15,24 @@ canvas.addEventListener('click', function (event) {
     var mouseY = event.clientY - rect.top;
 
     // Verifica se o clique ocorreu em um elemento interativo
-    if ((mouseX >= 326 && mouseX <= 369 && mouseY >= 77 && mouseY <= 98) ||
-        (mouseX >= 551 && mouseX <= 594 && mouseY >= 77 && mouseY <= 98)) {
+    if (mouseX >= 326 && mouseX <= 369 && mouseY >= 77 && mouseY <= 98) 
+        {
         // Exibe a modal
         var modal = document.getElementById('myModal');
         modal.style.display = "block";
     }
 
+    if (mouseX >= 551 && mouseX <= 594 && mouseY >= 77 && mouseY <= 98) 
+        {
+        // Exibe a modal
+        var modal = document.getElementById('myModal2');
+        modal.style.display = "block";
+    }
 });
 
 // Encontre o elemento de fechar da modal
 var closeModal = document.querySelector(".close");
+var closeModal2 = document.querySelector(".close2");
 
 // Adicione um evento de clique ao elemento de fechar
 closeModal.addEventListener("click", function () {
@@ -44,4 +40,11 @@ closeModal.addEventListener("click", function () {
     var modal = document.querySelector(".modal");
     // Oculte a modal
     modal.style.display = "none";
+});
+
+closeModal2.addEventListener("click", function () {
+    // Encontre a modal atual
+    var modal2 = document.querySelector(".modal2");
+    // Oculte a modal
+    modal2.style.display = "none";
 });
